7d9174c293f80631969ac6f9b70d785a3043542f
HLZ <huanglizhu412@gmail.com>
2020-11-10 09:33:12 +0800

    feat：添加了basename后github page的首页路径正常了，但是本地项目不对了

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 // import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/ChuangXiang">
     <App />
   </BrowserRouter>,
   document.getElementById('root'));
